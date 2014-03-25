d1e3434069cb41fdf3061625a34318800aa72078
Paul Welch <paul@pwelch.net>
2014-03-25 14:44:41 -0400

    Add method for ChefSpec Chef-Zero Data Bags
    ChefSpec expects the Chef-Zero Data Bags to be a single hash of
    all data bag items. This adds a method to create a single hash from
    all data bag fixtures.
    
    [26141]

--- lib/fauxbag/load_databag_json.rb
+++ lib/fauxbag/load_databag_json.rb
@@ -13,6 +13,26 @@ module Fauxbag
     end
   end
 
+  # Parse all data bag JSON files into a single hash
+  #
+  # @param [String] Data Bag Directory
+  # @return [Hash] All data bags in a single Hash
+  def load_chefzero_databag(databag)
+    databag_hash = Hash.new
+    databag_dir  = Dir.glob("#{data_bag_load_path}/#{databag}/" + '*.json')
+
+    databag_dir.each do |item|
+      File.open(item, "r") do |f|
+        data_bag_item = MultiJson.load(f)
+        id = data_bag_item.delete("id")
+
+        databag_hash[id] = data_bag_item
+      end
+    end
+
+    databag_hash
+  end
+
   private
 
   def data_bag_load_path
--- spec/load_databag_json_spec.rb
+++ spec/load_databag_json_spec.rb
@@ -6,3 +6,22 @@ describe '.load_databag_json' do
     expect(data_bag).to be_a Hash
   end
 end
+
+describe '.load_chefzero_databag' do
+  let(:all_databags_hash) {
+    {
+      "bar" => {"username"=>"bar"},
+      "foo" => {"username"=>"foobar"}
+    }
+  }
+
+  it 'returns a Hash' do
+    data_bag = load_chefzero_databag('test')
+    expect(data_bag).to be_a Hash
+  end
+
+  it 'returns a hash containing all data bag items' do
+    data_bag_hash = load_chefzero_databag('test')
+    expect(data_bag_hash).to eq all_databags_hash
+  end
+end
